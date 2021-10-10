07a9b00d14b3179a212991cc1a53bc3c605e9967
Isaac Yeboah <yeboah.determined.isaac@gmail.com>
2021-10-10 17:38:24 +0000

    update

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const getUsers = async () => {
+    const res = await fetch('/api/users');
+    const users = await res.json();
+//    console.log(users);
+    console.log(users);
+    users.forEach(function(user) {
+    // users.foreach( function (user) {
+        const li = document.createElement('li');
+        const text = document.createTextNode(user.name);
+        li.appendChild(text);
+        document.getElementById('users').appendChild(li);
+    }); 
+ 
+
+}
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".home",
+        start: "0%",
+        end: "60%",
+        scrub: 1,
+    }
+});
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".home",
+        start: "0%",
+        end: "60%",
+        scrub: 1,
+    }
+});
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".home",
+        start: "0%",
+        end: "200%",
+        scrub: 1,
+        pin: true,
+        pinSpacing: false
+    }
+});
+
+tl.fromTo(".sliding-text", { y: 0 }, { y: -400})
+tl2.fromTo(".logo", { scale: 6}, { scale: 1, top: "2rem", left: "3rem", x: "50%", y: "50%"})
+
